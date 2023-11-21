4adc5a69d25d7e2193e7d83ff58f91dcea3a8a38
Emran1233 <142645331+Emran1233@users.noreply.github.com>
2023-11-20 18:50:32 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,25 @@ import Banner from "./components/Banner";
 import {useState} from "react";
 import satData from "./components/satData"; 
 
-const filterByType = (currentType) => {
+function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
   const displaySats = satData.filter((newSatDisplay) => {
      return newSatDisplay.orbitType === currentType;
   });
   setSat(displaySats);
 };
-
-function App() {
-  const [sat, setSat] = useState(satData);
-  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
   return (
-    <div>
+    <>
       <Buttons 
-         filterByType={filterByType}
          setSat={setSat}
          displaySats={displaySats}
+         filterByType={filterByType}
       />
       <Banner/>
       <Table sat={sat} />
-    </div>
+    </>
   );
 }
 
